2bed6cadc59e22b359a4b05bcb2dea898b173a40
Kevin Doran <kev.doran@gmail.com>
2018-09-27 17:49:52 +0100

    wip: id search

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,24 +2,44 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { parseUrl } from 'utils/utils'
 import fetcher from 'utils/fetcher'
+import Modal from './Modal'
+import SearchContainer from '../containers/SearchContainer'
 
 import './nav.css'
 
 export default class extends React.Component {
+  state = { showSearchModal: true }
+
+  toggleSearchModal = (e) => {
+    if (e) e.preventDefault()
+    this.setState({ showSearchModal: !this.state.showSearchModal })
+  }
+
   render () {
-    const { userCtx, match: { params: { couch } } } = this.props
+    const { userCtx, match: { params: { couch, dbName } } } = this.props
+    const { showSearchModal } = this.state
     const couchUrl = parseUrl(couch)
+
     return (
       <div className='nav-container'>
         <div className='nav'>
           <span className='nav-left'>
-            CouchDB Lookout | <Link to='/'>change couch</Link>
+            CouchDB Lookout | <Link to='/'>change couch</Link> {dbName && (
+              <span> | <a href='' onClick={this.toggleSearchModal}>search</a></span>
+            )}
           </span>
           <span className='nav-right'>
             user: <Link to={`/${couch}/_users/org.couchdb.user:${encodeURIComponent(userCtx.name)}`}>{userCtx.name}</Link> |&nbsp;
             <a href='#' onClick={() => fetcher.destroySession(couchUrl)}>logout</a>
           </span>
         </div>
+        <Modal
+          show={showSearchModal}
+          onClose={this.toggleSearchModal}
+          className='search-modal'
+        >
+          <SearchContainer />
+        </Modal>
       </div>
     )
   }
