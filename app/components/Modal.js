d7f928329d69e0285a397ec62a3888a3051da070
Theophilus Omorebee <theo4u@ymail.com>
2018-09-28 00:20:16 +0100

    feat: esc to close modal

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -4,9 +4,16 @@ import './modal.css'
 
 export default function Modal ({ show, onClose, heading, children, className }) {
   const classes = 'modal ' + (className || '')
+
+  const handleOnKeyDown = event => {
+    if (event.key === 'Escape') {
+      onClose(event)
+    }
+  }
+
   return show
     ? <ClickOutHandler onClickOut={onClose}>
-      <div className={classes}>
+      <div className={classes} onKeyDown={handleOnKeyDown} tabIndex='0'>
         <div>
           <button className='close' onClick={onClose}><span>×</span></button>
           <h5>{heading}</h5>
