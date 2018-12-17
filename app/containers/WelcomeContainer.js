41dfb5ff9feae400b315c5569013abe777b56d75
Olamuhammed <damolagunju@gmail.com>
2018-12-17 09:49:40 +0100

    Changes-made

--- app/containers/WelcomeContainer.js
+++ app/containers/WelcomeContainer.js
@@ -1,15 +1,12 @@
-import React, {Component} from  'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react'
 
-
-class Welcome extends Component{
-  render(){
-    return(
+class Welcome extends Component {
+  render () {
+    return (
       <div>
-      <h1>Welcome</h1>
+        <h1>Welcome</h1>
       </div>
     )
   }
 }
-export default Welcome;
+export default Welcome
