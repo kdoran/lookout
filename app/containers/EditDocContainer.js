1b1fd1296e3a0bd652741abf1345c2bfe873aa58
Kevin Doran <kev.doran@gmail.com>
2019-04-11 16:32:10 +0100

    fix: standard

--- app/containers/EditDocContainer.js
+++ app/containers/EditDocContainer.js
@@ -57,17 +57,16 @@ export default class extends React.Component {
 
   onSubmit = () => {
     const { couchUrl, dbName, couch } = this.props
-    const { input, isNew, docId  } = this.state
+    const { input, isNew } = this.state
 
     const jsObjectInput = JSON.parse(input)
     this.setState({ saving: true }, () => {
       // New documents will get ID from input
       const docId = isNew ? jsObjectInput._id : this.state.docId
       fetcher.dbPut(couchUrl, dbName, docId, jsObjectInput).then(() => {
-        this.setState({ docId }, () => {this.props.history.push(`/${couch}/${dbName}/${docId}`)})
+        this.setState({ docId }, () => { this.props.history.push(`/${couch}/${dbName}/${docId}`) })
       }).catch(error => this.setState({ error, saving: false }))
-    });
-
+    })
   }
 
   deleteDoc = () => {
@@ -91,7 +90,6 @@ export default class extends React.Component {
       this.props.history.push(`/${couch}/${dbName}/${docId}`)
     }
   }
-
 
   render () {
     const { couch, couchUrl, dbName } = this.props
