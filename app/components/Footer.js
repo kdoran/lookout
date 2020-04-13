ec9642069e268a09487ff139c505300a7dca1749
Kevin Doran <kev.doran@gmail.com>
2020-04-13 12:32:15 +0100

    footer test container

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { parseUrl } from 'utils/utils'
-import fetcher from 'utils/fetcher'
 import Modal from './Modal'
 import SearchContainer from '../containers/SearchContainer'
 
